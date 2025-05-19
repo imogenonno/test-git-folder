a4eff51d1c0281fff6602837c3a5f3160d150b12
imogenonno <imogenonno@gmail.com>
2025-05-19 11:07:12 +0100

    added all files but using git ignore

--- test_push.R
+++ test_push.R
@@ -4,7 +4,8 @@ dim(df)
 df <- data.frame(height = c(22,33,131,42141),
                  weight = c(34,45,67,88))
 
-saveRDS(df, "/Users/imogenonno/test-git-folder/data.rds")
-saveRDS(df, "/Users/imogenonno/test-git-folder/data_folder/hidden_data2.rds")
+saveRDS(df, "/Users/imogenonno/test-git-folder/data2.rds")
+saveRDS(df, "/Users/imogenonno/test-git-folder/data_folder/hidden_data3.rds")
 
 #a change
+#another change
